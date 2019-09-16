426b6c9365f021092f0bb718e031aa72fcbd8a0c
Hitesh Pant <hiteshpantktm@gmail.com>
2019-09-16 18:25:57 +0530

    Initial commit
    Read and write
    commented code can also be used please check it to.

---
+++ firebase.js
@@ -0,0 +1,73 @@
+var admin = require('firebase-admin');
+
+var serviceAccount = require("D:\/node\/serviceAccountKey.json");
+
+var app = admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://experiment-1536292725060.firebaseio.com"
+});
+
+var db = admin.database();
+var ref = db.ref("D:\/node\/fireblog");
+
+var usersRef = ref.child("users");
+
+
+//  INSERT / UPDATE
+usersRef.set({
+  alanisawesome: {
+    date_of_birth: "June 23, 1912",
+    full_name: "Alan Turing"
+  },
+  gracehop: {
+    date_of_birth: "December 9, 1906",
+    full_name: "Grace Hopper"
+  }
+});
+
+// INSERT / APPEND
+
+// usersRef.child("alanisawesome").set({
+//   date_of_birth: "June 23, 1912",
+//   full_name: "Alan Turing"
+// });
+// usersRef.child("gracehop").set({
+//   date_of_birth: "December 9, 1906",
+//   full_name: "Grace Hopper"
+// });
+
+
+//  UPDATE SINGLE
+
+/*
+var hopperRef = usersRef.child("gracehop");
+hopperRef.update({
+  "nickname": "Amazing Grace"
+});
+
+
+*/
+
+//  UPDATE MULTIPLE
+/*
+usersRef.update({
+  "alanisawesome/nickname": "Alan The Machine",
+  "gracehop/nickname": "Amazing Grace"
+});
+*/
+
+var postsRef = ref.child("posts");
+var postId = postsRef.key;
+// postsRef.push().set({
+//   author: "alanisawesome",
+//   title: "The Turing Machine"
+// });
+
+postsRef.push({
+  author: "gracehop",
+  title: "Announcing COBOL, a New Programming Language"
+});
+
+
+
+console.log( 'ok', postId)
---
+++ firebaseRead.js
@@ -0,0 +1,41 @@
+var admin = require('firebase-admin');
+
+var serviceAccount = require("D:\/node\/serviceAccountKey.json");
+
+var app = admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://experiment-1536292725060.firebaseio.com"
+});
+
+var db = admin.database();
+var ref = db.ref("D:\/node\/fireblog/posts");
+
+// var usersRef = ref.child("users");
+
+// reading db
+
+/*
+ref.on("value", function(snapshot) {
+  console.log(snapshot.val());
+}, function (errorObject) {
+  console.log("The read failed: " + errorObject.code);
+});
+*/
+
+var postsRef = ref.child("posts");
+
+// reading db on new insertion
+/*
+postsRef.push({
+  author: "gracehop",
+  title: "Announcing COBOL, a New Programming Language"
+});
+
+
+ref.on("child_added", function(snapshot, prevChildKey) {
+  var newPost = snapshot.val();
+  console.log("Author: " + newPost.author);
+  console.log("Title: " + newPost.title);
+  console.log("Previous Post ID: " + prevChildKey);
+});
+*/
